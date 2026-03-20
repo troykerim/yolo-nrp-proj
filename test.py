2b84ead9f00fe0b54c6d76e14fcfc79f787d43eb
troykerim <troykerim@gmail.com>
2026-03-20 14:10:19 -0700

    Updating test paths

--- test.py
+++ test.py
@@ -18,22 +18,22 @@ CHANGE PATHS!!! espicially input and output folders!  Double check YOLO location
 '''
 
 def main():
-    # Path to trained YOLOv11 weights on PVC  
+    # Path to trained YOLOv11 weights on PVC  (Best model)
     model = YOLO(
-        "/workspace/yolo-output/yolov11-3rd/train_session8/weights/best.pt" 
+        "/workspace/yolo-output/yolov11/train_session2/weights/best.pt"
         # /workspace/output/yolov11-2nd/train_session2/weights/best.pt"  
     )
 
     # Folder containing test images
     input_folder = (
-        "/workspace/data/jam-material-YOLO/test/images"
+        "/workspace/data/Jam-Causing-Material.v1i.yolov11-3-18/test/images"
         # "/workspace/data/yolov11-Feb11th-dataset/test/images" 
         # "/workspace/data/jam-causing-material-CURRENT.yolov11/test/images"
     )
 
     # Folder where predictions will be saved
     output_folder = (
-        "/workspace/yolo-predictions/predictions-feb-24"
+        "/workspace/yolo-predictions/predictions-mar-20"
         # "/workspace/yolo-predictions/predictions-feb-12" 
     )  
 
