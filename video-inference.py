3cdd941c63fe59ab4d8c79a3cdd4ea1a021bb997
troykerim <troykerim@gmail.com>
2026-03-25 10:24:29 -0700

    Creating video inference job yaml

--- video-inference.py
+++ video-inference.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 model = YOLO("/workspace/yolo-output/yolov11-3-21/train_session2/weights/best.pt")
 
 model.predict(
-    source="/workspace/data/videos/moment_sort_upper_2026-01-28T04_30_51.mp4",  # your .mov file
+    source="/workspace/data/videos/moment_sort_upper_2026-01-28T04_30_51.mp4",  
     save=True,
     conf=0.25,
     device=0,
