9e7107a600655a7b8078f83fe422e9aa81a4d2c5
troykerim <troykerim@gmail.com>
2026-03-31 08:57:17 -0700

    Changing confidence score

--- video-inference.py
+++ video-inference.py
@@ -5,7 +5,7 @@ model = YOLO("/workspace/yolo-output/yolov11-3-21/train_session2/weights/best.pt
 model.predict(
     source="/workspace/data/videos/moment_sort_upper_2026-01-28T04_30_51.mp4",  
     save=True,
-    conf=0.25,
+    conf=0.2,       # 0.25 defualt
     device=0,
     project="/workspace/yolo-output",
     name="video_test-3-25"
